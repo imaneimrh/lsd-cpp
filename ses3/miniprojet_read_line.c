8ddd734c6b4919d5ea1d8398948955ad33b2a4bc
imaneimrh <92060050+imaneimrh@users.noreply.github.com>
2022-11-11 15:40:18 +0100

    submit project
    submit project

---
+++ ses3/miniprojet_read_line.c
@@ -0,0 +1,224 @@
+# include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+//-----------------------------------------------------------PROTOTYPE------------------------------------------------------------
+char	*get_next_line(int fd);
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+//---------------------------------------------------------AUXILARY FUNCTIONS------------------------------------------------------
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *left_str, char *buff)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!left_str)
+	{
+		left_str = (char *)malloc(1 * sizeof(char));
+		left_str[0] = '\0';
+	}
+	if (!left_str || !buff)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen(left_str) + ft_strlen(buff)) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (left_str)
+		while (left_str[++i] != '\0')
+			str[i] = left_str[i];
+	while (buff[j] != '\0')
+		str[i++] = buff[j++];
+	str[ft_strlen(left_str) + ft_strlen(buff)] = '\0';
+	free(left_str);
+	return (str);
+}
+
+/*  GET_LINE
+/	---------
+*	Extracts the line (ending in either line break and '\0' or only '\0' in EOF)
+*	from static variable.
+*	PARAMETERS
+*	#1. The pointer to the cumulative static variable from previous runs of read_line.
+*	RETURN VALUES
+*	The string with the full line ending in a line break ('\n') + ('\0').
+*	-------------
+*/
+char	*ft_get_line(char *left_str)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!left_str[i])
+		return (NULL);
+	while (left_str[i] && left_str[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (left_str[i] && left_str[i] != '\n')
+	{
+		str[i] = left_str[i];
+		i++;
+	}
+	if (left_str[i] == '\n')
+	{
+		str[i] = left_str[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/*	NEW_LEFT_STR
+*	-------------
+*	Stores in the cumulative static variable the new updated variable with whatever
+*	is left from the original, minus the line extracted.
+*	PARAMETERS
+*	#1. The pointer to the cumulative static variable from previous runs of read_line.
+*	RETURN VALUES
+*	The new updated string with whatever is left from the original static, minus the
+*	line extracted.
+*/
+char	*ft_new_left_str(char *left_str)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (left_str[i] && left_str[i] != '\n')
+		i++;
+	if (!left_str[i])
+	{
+		free(left_str);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(left_str) - i + 1));
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (left_str[i])
+		str[j++] = left_str[i++];
+	str[j] = '\0';
+	free(left_str);
+	return (str);
+}
+//----------------------------------------------------------
+/*	READ_TO_LEFT_STR
+*	-----------------
+*	DESCRIPTION
+*	Takes the opened file descriptor and saves on a "buff" variable what readed
+*	from it. Then joins it to the cumulative static variable for the persistence
+*	of the information.
+*	PARAMETERS
+*	#1. A file descriptor.
+*	#2. The pointer to the cumulative static variable from previous runs of
+*	read_line.
+*	RETURN VALUES
+*	The new static variable value with buffer joined for the persistence of the info,
+*	or NULL if error.
+*/
+char	*ft_read_to_left_str(int fd, char *left_str)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return (left_str);
+}
+
+
+
+//---------------------------------------------------------READ_LINE()-------------------------------------------------------------
+char	*read_line(int fd)
+{
+	char		*line;
+	static char	*left_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	left_str = ft_read_to_left_str(fd, left_str);
+	if (!left_str)
+		return (NULL);
+	line = ft_get_line(left_str);
+	left_str = ft_new_left_str(left_str);
+	return (line);
+}
+//---------------------------------------------------------TEST in MAIN()-----------------------------------------------------------
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("tester.txt", O_RDONLY);
+	while (1)
+	{
+		line = read_line(fd);
+		printf("%s", line);
+		if (line == NULL)
+			break ;
+		free(line);
+		
+	}
+	return (0);
+}
